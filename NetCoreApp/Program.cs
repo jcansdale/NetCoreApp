ca4f45f7e4e2379b912ee3e288ef066c48bde09d
Jamie Cansdale <jcansdale@github.com>
2019-11-27 09:36:21 +0000

    Add project files.

---
+++ NetCoreApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace NetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.SayHello();
+        }
+    }
+}
